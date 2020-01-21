6fc5398a2468bfdd113004fbc2f7122129dda511
EtienneThompson <et@etiennethompson.com>
2020-01-21 15:51:31 -0700

    Removed log file dependency.
    core_specifics_template.py
    	- Added ClarityApi setup to remove need to continually dig through
    		object attributes until finding ClarityApi.
    
    project_transfer.py
    	- Removed dependency on the log file to determine which projects
    		needed to be moved. Now it just looks in Clarity for any active
    		iLab requests, and tries to move the ones that are not already
    		in Clarity.
    	- Removed get_project_history function, which scraped information
    		from the log but is now no longer necessary.

--- ua_project_transfer/core_specifics_template.py
+++ ua_project_transfer/core_specifics_template.py
@@ -5,6 +5,7 @@ import os
 import logging
 import json
 from ua_ilab_tools import ua_ilab_tools
+from ua_clarity_api import ua_clarity_api
 from ua_project_transfer import log_config_template
 from ua_project_transfer import project_lims_tools
 # from ua_project_transfer import next_steps
@@ -49,3 +50,16 @@ def setup_ilab_api(env):
     with open(creds_path, 'r') as file:
         creds = json.load(file)
     return ua_ilab_tools.IlabTools(creds["core_id"], creds["token"])
+
+
+def setup_clarity_api(env):
+    """Initialize a ClarityApi object with a json creds file."""
+    creds_path = (os.path.join(
+        os.path.split(__file__)[0], "lims_token.json"))
+    with open(creds_path, 'r') as file:
+        creds = json.load(file)
+
+    return ua_clarity_api.ClarityApi(
+        host=creds["host"],
+        username=creds["username"],
+        password=creds["password"])
--- ua_project_transfer/project_transfer.py
+++ ua_project_transfer/project_transfer.py
@@ -1,11 +1,11 @@
 """Converts unprocessed ilab requests to Clarity projects."""
 import os
-import re
 import argparse
 import logging
 import traceback
 import datetime
 import requests
+from bs4 import BeautifulSoup
 from dataclasses import dataclass, field
 from ua_ilab_tools import ua_ilab_tools
 from ua_project_transfer import core_specifics
@@ -29,48 +29,6 @@ class ProjectRecord():
     date: datetime.datetime = field(default_factory=datetime.datetime.now)
 
 
-def get_project_history(log_file_name):
-    """Harvest all of the uri's from the given log.
-
-        Arguments:
-            log_file_name (string): The path to the log file. If the file is
-                in cwd, then it can be just the file name.
-
-        Returns:
-            processed_projects (set): The dict that holds all of the uri's
-                that the script should skip (either they have been processed
-                or an hour hasn't elapsed between the last failure).
-    """
-    processed_projects = set()
-    error_entries = dict()
-    with open(log_file_name, 'r') as file:
-        for line in file.readlines():
-            # Skip any line without a ProjectRecord.
-            if "uri=" not in line:
-                continue
-
-            current_uri = line.split("uri=")[1].split()[0].strip("',")
-            current_uri = re.sub(r"[^0-9]", '', current_uri)
-
-            if "INFO" in line:
-                processed_projects.add(current_uri)
-
-            elif "ERROR" in line:
-                log_time = line.split("datetime(")[1].split("))")[0]
-                attempt_datetime = datetime.datetime.strptime(
-                    log_time, "%Y, %m, %d, %H, %M, %S, %f")
-                error_entries.setdefault(current_uri, list())
-                error_entries[current_uri].append(attempt_datetime)
-
-    for uri in error_entries:
-        max_datetime = max(error_entries[uri])
-        if (datetime.datetime.now() - max_datetime
-                < datetime.timedelta(hours=1)):
-            processed_projects.add(uri)
-
-    return processed_projects
-
-
 def main():
     # Set up the arguments with names for the different environments.
     parser = argparse.ArgumentParser()
@@ -82,6 +40,7 @@ def main():
 
     lims_utility = core_specifics.setup_lims_api(args.lims_env)
     ilab_tools = core_specifics.setup_ilab_api(args.ilab_env)
+    clarity_api = core_specifics.setup_clarity_api(args.lims_env)
 
     # Setup the charge pricing scheme.
     charge_per_reaction = {
@@ -97,163 +56,167 @@ def main():
         all_req_ids = {}
 
     # To process the entire history, regardless of the year in the file name.
-    to_process = all_req_ids.keys()
+    to_process = [key for key in all_req_ids.keys()]
 
-    min_year = datetime.datetime.today().year
-    directory = sorted(os.listdir())
-    for elem in directory:
-        if elem.startswith("project_transfer_log_"):
-            min_year = int(re.sub(r"[^0-9]", "", elem))
-            break
+    # Query for all of the projects in to_process
+    clarity_projects = BeautifulSoup(
+        clarity_api.get(
+            f"{clarity_api.host}projects", parameters={"name": to_process}),
+        "xml")
 
-    # The min_year is this year, unless a smaller year has been found.
-    for year in range(min_year, datetime.datetime.today().year + 1):
-        current_log_name = f"project_transfer_log_{year}.txt"
-        to_process = set(
-            to_process).difference(get_project_history(current_log_name))
+    clarity_prj_ids = {prj.find("name").text: prj[
+        "limsid"] for prj in clarity_projects.find_all("project")}
 
     for req_id in to_process:
-        req_soup = all_req_ids[req_id]
-        request_type = req_soup.find("category-name").string
-        # If a log_config has initialized these handlers, reset the toaddrs of
-        # those handlers. If there are not handlers with those names or that
-        # attribute, pass.
-        try:
-            warn_index = LOGGER.handlers.index("warn_handler")
-            error_index = LOGGER.handlers.index("error_handler")
-            LOGGER.handlers[warn_index].toaddrs = core_specifics.get_email(
-                request_type)
-            LOGGER.handlers[error_index].toaddrs = core_specifics.get_email(
-                request_type)
-        except (ValueError, AttributeError):
-            pass
-
-        prj_info = ua_ilab_tools.extract_project_info(req_soup)
-        current_record = ProjectRecord(uri=prj_info.name)
-        try:
-            forms_uri_to_soup = ilab_tools.get_custom_forms(req_id)
-
-        except ua_ilab_tools.IlabConfigError:
-            # Requests without forms are Consultation Requests or Custom
-            # Projects that have not yet had a business form attached, and
-            # so the request should just be skipped.
-            continue
-        # Make sure the current_form is zeroed out between request id's.
-        else:
-            current_form = None
-
-        # Get all of the custom forms for the current req_id.
-        for form_uri, form_soup in forms_uri_to_soup.items():
-            # Get the form info (including the sample info) before you post
-            # anything, as this is where most of the errors are thrown.
+        if req_id not in clarity_prj_ids.keys():
+            # Gather all of the project information from iLab if the project is
+            # not already in Clarity.
+            req_soup = all_req_ids[req_id]
+            request_type = req_soup.find("category-name").string
+            # If a log_config has initialized these handlers, reset the toaddrs
+            # of those handlers. If there are not handlers with those names or
+            # that attribute, pass.
             try:
-                current_form = ua_ilab_tools.extract_custom_form_info(
-                    req_id, form_uri, form_soup)
+                warn_index = LOGGER.handlers.index("warn_handler")
+                err_index = LOGGER.handlers.index("error_handler")
+                LOGGER.handlers[warn_index].toaddrs = core_specifics.get_email(
+                    request_type)
+                LOGGER.handlers[err_index].toaddrs = core_specifics.get_email(
+                    request_type)
+            except (ValueError, AttributeError):
+                pass
 
-            except TypeError as grid_error:
-                grid_error = str(grid_error).replace('"', '\'')
+            prj_info = ua_ilab_tools.extract_project_info(req_soup)
+            current_record = ProjectRecord(uri=prj_info.name)
+            try:
+                forms_uri_to_soup = ilab_tools.get_custom_forms(req_id)
+
+            except ua_ilab_tools.IlabConfigError:
+                # Requests without forms are Consultation Requests or Custom
+                # Projects that have not yet had a business form attached, and
+                # so the request should just be skipped.
+                continue
+            # Make sure the current_form is zeroed out between request id's.
+            else:
+                current_form = None
+
+            # Get all of the custom forms for the current req_id.
+            for form_uri, form_soup in forms_uri_to_soup.items():
+                # Get the form info (including the sample info) before you post
+                # anything, as this is where most of the errors are thrown.
+                try:
+                    current_form = ua_ilab_tools.extract_custom_form_info(
+                        req_id, form_uri, form_soup)
+
+                except TypeError as grid_error:
+                    grid_error = str(grid_error).replace('"', '\'')
+                    LOGGER.error({
+                        "template": os.path.join(
+                            "project_transfer", "error.html"),
+                        "content": (
+                            f"The request {current_record} has been filled out"
+                            f" incorrectly. The error message is:\n"
+                            f"{grid_error}")
+                    })
+                    break
+
+                # Add the request_type to the form that was just added.
+                current_form.request_type = request_type
+
+                # If the request only has a 'Request a Quote' form, continue.
+                if "REQUEST A QUOTE" in current_form.name.strip().upper():
+                    current_form = None
+                    continue
+
+            # If there was an error with the business form, don't try to post
+            # info that is not there.
+            if current_form is None:
+                continue
+
+            # Try to post every part of a request, but if it fails, delete what
+            # was made.
+
+            delete_list = list()
+            try:
+                res_uri = lims_utility.create_researcher(req_id, prj_info)
+                delete_list.append(res_uri)
+                prj_uri = lims_utility.create_project(req_id, prj_info)
+                delete_list.append(prj_uri)
+
+                con_uris = lims_utility.create_containers(
+                    req_id, prj_info, current_form)
+                delete_list.extend(con_uris)
+
+                sample_uris = lims_utility.create_samples(
+                    req_id, prj_info, current_form)
+                delete_list.extend(sample_uris)
+
+            except BaseException as post_error:
                 LOGGER.error({
                     "template": os.path.join("project_transfer", "error.html"),
                     "content": (
                         f"The request {current_record} has been filled out"
-                        f" incorrectly. The error message is:\n{grid_error}")
+                        f" incorrectly. The error message is:\n {post_error}")
                 })
-                break
+                # Delete in reverse order, so Clarity allows you to delete
+                # things.
+                for item in delete_list[::-1]:
+                    try:
+                        lims_utility.lims_api.tools.api.delete(item)
+                    except requests.exceptions.HTTPError:
+                        continue
 
-            # Add the request_type to the form that was just added.
-            current_form.request_type = request_type
-
-            # If the request only has a 'Request a Quote' form, continue.
-            if "REQUEST A QUOTE" in current_form.name.strip().upper():
-                current_form = None
-                continue
-
-        # If there was an error with the business form, don't try to post info
-        # that is not there.
-        if current_form is None:
-            continue
-
-        # Try to post every part of a request, but if it fails, delete what was
-        # made.
-        delete_list = list()
-        try:
-            res_uri = lims_utility.create_researcher(req_id, prj_info)
-            delete_list.append(res_uri)
-            prj_uri = lims_utility.create_project(req_id, prj_info)
-            delete_list.append(prj_uri)
-
-            con_uris = lims_utility.create_containers(
-                req_id, prj_info, current_form)
-            delete_list.extend(con_uris)
-
-            sample_uris = lims_utility.create_samples(
-                req_id, prj_info, current_form)
-            delete_list.extend(sample_uris)
-
-        except BaseException as post_error:
-            LOGGER.error({
-                "template": os.path.join("project_transfer", "error.html"),
-                "content": (
-                    f"The request {current_record} has been filled out"
-                    f" incorrectly. The error message is:\n {post_error}")
-            })
-            # Delete in reverse order, so Clarity allows you to delete things.
-            for item in delete_list[::-1]:
+            else:
+                # Collect and compare the expected and actual prices.
                 try:
-                    lims_utility.lims_api.tools.api.delete(item)
-                except requests.exceptions.HTTPError:
-                    continue
+                    if request_type in charge_per_reaction.keys():
+                        req_price, calcd_price = price_check.check_request(
+                            ilab_tools,
+                            req_id,
+                            request_type,
+                            current_form.samples,
+                            rxn_multiplier=charge_per_reaction[request_type])
+                    else:
+                        req_price, calcd_price = price_check.check_request(
+                            ilab_tools,
+                            req_id,
+                            request_type,
+                            current_form.samples)
 
-        else:
-            # Collect and compare the expected and actual prices.
-            try:
-                if request_type in charge_per_reaction.keys():
-                    req_price, calculated_price = price_check.check_request(
-                        ilab_tools,
-                        req_id,
-                        request_type,
-                        current_form.samples,
-                        rxn_multiplier=charge_per_reaction[request_type])
-                else:
-                    req_price, calculated_price = price_check.check_request(
-                        ilab_tools,
-                        req_id,
-                        request_type,
-                        current_form.samples)
+                    current_record.actual_price = req_price
+                    current_record.expected_price = calcd_price
 
-                current_record.actual_price = req_price
-                current_record.expected_price = calculated_price
+                # Catch BaseException here because I don't want the program to
+                # ever stop here if price_check throws an error.
+                except BaseException:
+                    LOGGER.warning({
+                        "template": os.path.join(
+                            "project_transfer", "price_warning.html"),
+                        "content": (
+                            f"The project {req_id} prices could not be"
+                            f" checked. The traceback is:\n"
+                            f"{traceback.format_exc()}")
+                    })
 
-            # Catch BaseException here because I don't want the program to ever
-            # stop here if price_check throws an error.
-            except BaseException:
-                LOGGER.warning({
-                    "template": os.path.join(
-                        "project_transfer", "price_warning.html"),
-                    "content": (
-                        f"The project {req_id} prices could not be checked."
-                        f" The traceback is:\n{traceback.format_exc()}")
-                })
+                smp_art_uris = (
+                    lims_utility.lims_api.tools.get_arts_from_samples(
+                        sample_uris))
 
-            smp_art_uris = lims_utility.lims_api.tools.get_arts_from_samples(
-                sample_uris)
-            # Assign those samples to workflows.
-            try:
-                lims_utility.route_strategy(
-                    smp_art_uris.values(), current_form, args.lims_env)
+                # Assign those samples to workflows.
+                try:
+                    lims_utility.route_strategy(
+                        smp_art_uris.values(), current_form, args.lims_env)
 
-            # Catch BaseException here because I don't want the program to ever
-            # stop here if route_strategy throws an error.
-            except BaseException:
-                LOGGER.warning({
-                    "template": os.path.join(
-                        "project_transfer", "route_warning.html"),
-                    "content": (
-                        f"The project {req_id} could not be routed. The"
-                        f" traceback is:\n{traceback.format_exc()}")
-                })
-
-            LOGGER.info(current_record)
+                # Catch BaseException here because I don't want the program to
+                # ever stop here if route_strategy throws an error.
+                except BaseException:
+                    LOGGER.warning({
+                        "template": os.path.join(
+                            "project_transfer", "route_warning.html"),
+                        "content": (
+                            f"The project {req_id} could not be routed. The"
+                            f" traceback is:\n{traceback.format_exc()}")
+                    })
 
 
 if __name__ == '__main__':
